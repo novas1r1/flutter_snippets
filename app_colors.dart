feb1d08f6de1438d250b4d40ec5ae63a3967dd75
Verry <development@verena-zaiser.de>
2023-12-12 14:54:49 +0100

    Create app_colors.dart

---
+++ app_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  const AppColors._();
+
+  static const Color fontDark = Color(0xFF000000);
+  static const Color fontLight = Color(0xFFFFFFFF);
+  static const Color icon = Color(0xFF1C1C1C);
+
+  static const Color primary = Color(0xFFCC071E);
+  static const Color onPrimary = Color(0xFFFFFFFF);
+
+  static const Color secondary = Color(0xFFFFFFFF);
+  static const Color onSecondary = Color(0xFF1C1C1C);
+
+  static const Color tertiary = Color(0xFFE5EFF9);
+  static const Color onTertiary = Color(0xFF0065CB);
+
+  static const Color error = Color(0xFFCC071E);
+  static const Color onError = Color(0xFFFFFFFF);
+
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color onBackground = Color(0xFF1C1C1C);
+
+  static const Color surface = Color(0xFFFFFFFF);
+  static const Color onSurface = Color(0xFF1C1C1C);
+}
