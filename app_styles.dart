67a352730fcb0b5eb3d58d0419d3feffcf23c430
Verry <development@verena-zaiser.de>
2023-12-13 13:19:55 +0100

    Update app_styles.dart

--- app_styles.dart
+++ app_styles.dart
@@ -1,133 +1,134 @@
 import 'package:flutter/widgets.dart';
 
-abstract class AppTextStyle {
-  // m3DisplayLarge Text Style
-  static const TextStyle m3DisplayLarge = TextStyle(
+abstract class AppTextStyles {
+  static const displayLarge = TextStyle(
     fontSize: 57,
-    fontWeight: FontWeight.w400, // regular
-    height: 1.12, // 64px 
+    fontWeight: FontWeight.w400,
+    height: 1.12, // 64px
     letterSpacing: -0.25,
     fontFamily: 'Roboto',
   );
-  // m3DisplayMedium Text Style
-  static const TextStyle m3DisplayMedium = TextStyle(
+
+  static const displayMedium = TextStyle(
     fontSize: 45,
-    fontWeight: FontWeight.w400, // regular
-    height: 1.16, // 52px 
+    fontWeight: FontWeight.w400,
+    height: 1.16, // 52px
     fontFamily: 'Roboto',
   );
-  // m3DisplaySmall Text Style
-  static const TextStyle m3DisplaySmall = TextStyle(
+
+  static const displaySmall = TextStyle(
     fontSize: 36,
-    fontWeight: FontWeight.w400, // regular
-    height: 1.22, // 44px 
+    fontWeight: FontWeight.w400,
+    height: 1.22, // 44px
     fontFamily: 'Roboto',
   );
-  // m3HeadlineLarge Text Style
-  static const TextStyle m3HeadlineLarge = TextStyle(
+
+  static const headlineLarge = TextStyle(
     fontSize: 32,
-    fontWeight: FontWeight.w400, // regular
-    height: 1.25, // 40px 
+    fontWeight: FontWeight.w400,
+    height: 1.25, // 40px
     fontFamily: 'Roboto',
   );
-  // m3HeadlineMedium Text Style
-  static const TextStyle m3HeadlineMedium = TextStyle(
+
+  static const headlineMedium = TextStyle(
     fontSize: 28,
-    fontWeight: FontWeight.w400, // regular
-    height: 1.29, // 36px 
+    fontWeight: FontWeight.w400,
+    height: 1.29, // 36px
     fontFamily: 'Roboto',
   );
-  // m3HeadlineSmall Text Style
-  static const TextStyle m3HeadlineSmall = TextStyle(
+
+  static const headlineSmall = TextStyle(
     fontSize: 24,
-    fontWeight: FontWeight.w400, // regular
-    height: 1.33, // 32px 
+    fontWeight: FontWeight.w400,
+    height: 1.33, // 32px
     fontFamily: 'Roboto',
   );
-  // m3TitleLarge Text Style
-  static const TextStyle m3TitleLarge = TextStyle(
+
+  static const titleLarge = TextStyle(
     fontSize: 22,
-    fontWeight: FontWeight.w400, // regular
-    height: 1.27, // 28px 
+    fontWeight: FontWeight.w400,
+    height: 1.27, // 28px
     fontFamily: 'Roboto',
   );
-  // m3TitleMedium Text Style
-  static const TextStyle m3TitleMedium = TextStyle(
+
+  static const titleMedium = TextStyle(
     fontSize: 16,
-    fontWeight: FontWeight.w500, // medium
-    height: 1.5, // 24px 
+    fontWeight: FontWeight.w500,
+    height: 1.5, // 24px
     letterSpacing: 0.15,
     fontFamily: 'Roboto',
   );
-  // m3TitleSmall Text Style
-  static const TextStyle m3TitleSmall = TextStyle(
+
+  static const titleSmall = TextStyle(
     fontSize: 14,
-    fontWeight: FontWeight.w500, // medium
-    height: 1.43, // 20px 
+    fontWeight: FontWeight.w500,
+    height: 1.43, // 20px
     letterSpacing: 0.1,
     fontFamily: 'Roboto',
   );
-  // m3BodyLarge Text Style
-  static const TextStyle m3BodyLarge = TextStyle(
+
+  static const bodyLarge = TextStyle(
     fontSize: 16,
-    fontWeight: FontWeight.w400, // regular
-    height: 1.5, // 24px 
+    fontWeight: FontWeight.w400,
+    height: 1.5, // 24px
     letterSpacing: 0.5,
     fontFamily: 'Roboto',
   );
-  // m3BodyMedium Text Style
-  static const TextStyle m3BodyMedium = TextStyle(
+
+  static const bodyMedium = TextStyle(
     fontSize: 14,
-    fontWeight: FontWeight.w400, // regular
-    height: 1.43, // 20px 
+    fontWeight: FontWeight.w400,
+    height: 1.43, // 20px
     letterSpacing: 0.25,
     fontFamily: 'Roboto',
   );
-  // m3BodySmall Text Style
-  static const TextStyle m3BodySmall = TextStyle(
+
+  static const bodySmall = TextStyle(
     fontSize: 12,
-    fontWeight: FontWeight.w400, // regular
-    height: 1.33, // 16px 
+    fontWeight: FontWeight.w400,
+    height: 1.33, // 16px
     fontFamily: 'Roboto',
   );
-  // m3LabelLargeProminent Text Style
-  static const TextStyle m3LabelLargeProminent = TextStyle(
+
+  static const labelLargeProminent = TextStyle(
     fontSize: 14,
-    fontWeight: FontWeight.w600, // semi bold
-    height: 1.43, // 20px 
+    fontWeight: FontWeight.w600,
+    height: 1.43, // 20px
     letterSpacing: 0.1,
     fontFamily: 'Roboto',
   );
-  // m3LabelLarge Text Style
-  static const TextStyle m3LabelLarge = TextStyle(
+
+  static const labelLarge = TextStyle(
     fontSize: 14,
-    fontWeight: FontWeight.w500, // medium
-    height: 1.43, // 20px 
+    fontWeight: FontWeight.w500,
+    height: 1.43, // 20px
     letterSpacing: 0.1,
     fontFamily: 'Roboto',
   );
-  // m3LabelMediumProminent Text Style
-  static const TextStyle m3LabelMediumProminent = TextStyle(
+
+  static const labelMediumProminent = TextStyle(
     fontSize: 12,
-    fontWeight: FontWeight.w600, // semi bold
-    height: 1.33, // 16px 
+    fontWeight: FontWeight.w600,
+    height: 1.33, // 16px
     letterSpacing: 0.5,
     fontFamily: 'Roboto',
   );
-  // m3LabelMedium Text Style
-  static const TextStyle m3LabelMedium = TextStyle(
+
+  static const labelMedium = TextStyle(
     fontSize: 12,
-    fontWeight: FontWeight.w500, // medium
-    height: 1.33, // 16px 
+    fontWeight: FontWeight.w500,
+    height: 1.33, // 16px
     letterSpacing: 0.5,
     fontFamily: 'Roboto',
   );
-  // m3LabelSmall Text Style
-  static const TextStyle m3LabelSmall = TextStyle(
+
+  static const labelSmall = TextStyle(
     fontSize: 11,
-    fontWeight: FontWeight.w500, // medium
-    height: 1.45, // 16px 
+    fontWeight: FontWeight.w500,
+    height: 1.45, // 16px
     letterSpacing: 0.5,
     fontFamily: 'Roboto',
   );
+
+  const AppTextStyles._();
 }
